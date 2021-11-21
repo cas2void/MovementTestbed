f8467f725b55fdcaafa1cc05d8eeaec63e1e5414
jcyan <jcyan@tencent.com>
2021-11-21 11:52:36 +0800

    Add sliding sphere game mode

---
+++ Plugins/CatlikeCodingMovement/Source/SlidingSphere/Public/SlidingSphereGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlidingSphereGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLIDINGSPHERE_API ASlidingSphereGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
